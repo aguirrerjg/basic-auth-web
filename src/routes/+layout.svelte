aa53093ea5febd8a652f053e7ded39010fd0d60b
aguirrerjg <aguirrerjg@gmail.com>
2023-08-12 20:51:24 -0500

    Updated package-lock.json and package.json to include sveltekit-superforms@0.8.7.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,22 @@
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import type { LayoutData } from './$types';
+  import { onMount } from "svelte"
+  import { invalidate } from "$app/navigation"
+	
+	export let data: LayoutData
+  $: ({ session, supabase } = data)
+  onMount(() => {
+    const {
+        data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, _session) => {
+        if (_session?.expires_at !== session?.expires_at) {
+            invalidate("supabase:auth")
+        }
+    })
+    return () => subscription.unsubscribe()
+  })
 </script>
 
 <!-- App Shell -->
